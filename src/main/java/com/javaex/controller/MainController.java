b1b02206a2123dcd53b10c5c73aa5cab1714f174
clz2024.red <clz2024.red@gmail.com>
2024-04-12 13:59:06 +0900

    a

---
+++ src/main/java/com/javaex/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.javaex.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+	@GetMapping(value="/")
+	public String index() {
+		System.out.println("MainController.index()");
+		return "foword:/index.html";
+	}
+}
