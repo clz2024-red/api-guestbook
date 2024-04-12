4f6f7a01adb8656c79b7f1b05cc23fcb8387239d
clz2024.red <clz2024.red@gmail.com>
2024-04-12 14:03:44 +0900

    fsd

--- src/main/java/com/javaex/controller/MainController.java
+++ src/main/java/com/javaex/controller/MainController.java
@@ -3,7 +3,7 @@ package com.javaex.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 
 	@GetMapping(value="/")
